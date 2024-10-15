15009683ca66888a8338c05bfe278aed8c095313
Danial Alborz <danialalborz3@gmail.com>
2024-10-15 07:18:59 -0700

    bug

--- JS/index.js
+++ JS/index.js
@@ -1,11 +1,34 @@
 const numbers = document.querySelectorAll('[data-number]');
+const numbers2 = document.querySelectorAll('[data-number]');
 const result = document.querySelector('.output');
+const operators = document.querySelectorAll('[data-operation]');
+const clear = document.querySelector('[data-clear]');
 
-let x = numbers;
-console.log(x);
+let firstNumber = "";
+let secondNumber = "";
 
-x.forEach(number => {
-    number.addEventListener('click', () => {
-        result.innerHTML += number.textContent;
-    })
+// 
+
+
+
+operators.forEach(operator => {
+    operator.addEventListener('click', () => {
+        if (mod2) {
+            firstNumber = parseFloat(result.textContent)
+            parseFloat(result.textContent) = firstNumber
+            mod2 = false;
+        } else if (!mod2) {
+            secondNumber = parseFloat(result.textContent)
+            parseFloat(result.textContent) = secondNumber
+            mod2 = true;
+        }
+
+        result.innerHTML += ` ${operator.textContent} `;
+
+    });
+})
+clear.addEventListener('click', () => {
+    firstNumber = 0;
+    secondNumber = 0;
+    result.innerHTML = '';
 });
