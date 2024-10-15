277d07f821a548223f75bd1d9bcb9954e39c9965
Danial Alborz <danialalborz3@gmail.com>
2024-10-15 11:43:24 -0700

    debug

--- JS/index.js
+++ JS/index.js
@@ -1,34 +1,41 @@
 const numbers = document.querySelectorAll('[data-number]');
-const numbers2 = document.querySelectorAll('[data-number]');
 const result = document.querySelector('.output');
 const operators = document.querySelectorAll('[data-operation]');
 const clear = document.querySelector('[data-clear]');
+const equal = document.querySelector('[data-equals]');
 
 let firstNumber = "";
 let secondNumber = "";
+let op = '';
 
-// 
+let mod2 = true;
 
-
+numbers.forEach(number => {
+    number.addEventListener('click', () => {
+        if (mod2) {
+            result.innerHTML += number.textContent;
+            firstNumber += number.textContent;
+            console.log('first number : ', firstNumber);
+        } else if (!mod2) {
+            result.innerHTML += number.textContent;
+            secondNumber += number.textContent;
+            console.log('second number : ', secondNumber);
+        }
+    })
+})
 
 operators.forEach(operator => {
     operator.addEventListener('click', () => {
-        if (mod2) {
-            firstNumber = parseFloat(result.textContent)
-            parseFloat(result.textContent) = firstNumber
-            mod2 = false;
-        } else if (!mod2) {
-            secondNumber = parseFloat(result.textContent)
-            parseFloat(result.textContent) = secondNumber
-            mod2 = true;
-        }
-
-        result.innerHTML += ` ${operator.textContent} `;
-
-    });
+        result.innerHTML += operator.textContent;
+        op += operator.textContent;
+        mod2 = !mod2;
+        console.log('operator : ', op);
+    })
 })
-clear.addEventListener('click', () => {
-    firstNumber = 0;
-    secondNumber = 0;
-    result.innerHTML = '';
-});
+
+equal.addEventListener('click', () => {
+    if (op === '+') {
+        let submit = parseFloat(firstNumber) + parseFloat(secondNumber);
+        result.innerHTML = submit;
+    }
+})
